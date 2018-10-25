54d81896a1fea99968d3ac8076974b0f33ae900e
hide.maeda <atari.maeda.no.kurakkaaa1173@gmail.com>
2018-10-25 15:27:51 +0000

    user add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:update, :destroy]
   
   def index
     @tasks = Task.all.page(params[:page])
@@ -13,12 +15,13 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'タスクが登録されませんでした'
       render :new
     end
@@ -30,7 +33,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスクが更新されませんでした'
       render :new
@@ -41,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
@@ -49,9 +52,16 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :user_id)
   end
 end
